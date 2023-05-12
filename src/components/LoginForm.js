14eef34ddd43d187badd6569b903f643704db302
11m245 <sivaraj2siva@gmail.com>
2023-05-12 09:04:47 +0530

    update loading button

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,9 +4,11 @@ import { toast } from "react-toastify";
 import * as yup from "yup";
 import "./Forms.css";
 import { Link, useNavigate } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { appContext } from "../App";
+import { CustomLoadingButton } from "./customLoadingButton";
 function LoginForm() {
+  const [isLoading, setIsLoading] = useState(false);
   const { currentUser, setCurrentUser, socket } = useContext(appContext);
   const navigate = useNavigate();
   const loginFormInitialValuesObj = { email: "", password: "" };
@@ -24,6 +26,7 @@ function LoginForm() {
     });
 
   const login = async (formValues) => {
+    setIsLoading(true);
     const isAnyEmptyFields = Object.keys(formValues).some(
       (key) => formValues[key] === ""
     );
@@ -41,6 +44,7 @@ function LoginForm() {
       );
       console.log("fetch Response login", fetchResponse);
       if (fetchResponse.status === 200) {
+        setIsLoading(false);
         const data = await fetchResponse.json();
         localStorage.setItem("logintoken", data.payload.token);
         localStorage.setItem("chatEmail", data.payload.email);
@@ -48,6 +52,7 @@ function LoginForm() {
         toast.success(data.message);
         navigate("/user");
       } else {
+        setIsLoading(false);
         const data = await fetchResponse.json();
         toast.error(data.message);
       }
@@ -75,9 +80,15 @@ function LoginForm() {
           onChange={handleChange}
           onBlur={handleBlur}
         />
-        <Button type="submit" variant="contained">
-          Login
-        </Button>
+        <CustomLoadingButton
+          isLoading={isLoading}
+          setIsLoading={setIsLoading}
+          buttonComponent={
+            <Button type="submit" variant="contained">
+              Login
+            </Button>
+          }
+        />
         <div className="other-links">
           <Link to="/forgot">Forgot?</Link>
           <Link to="/signup">New? Signup</Link>
